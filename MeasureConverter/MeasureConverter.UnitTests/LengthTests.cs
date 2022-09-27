d21923a593608acb89cea0f24eb02d4559df8362
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-09-27 22:28:07 +0200

    Update

--- MeasureConverter/MeasureConverter.UnitTests/LengthTests.cs
+++ MeasureConverter/MeasureConverter.UnitTests/LengthTests.cs
@@ -9,7 +9,7 @@ public class LengthTests
     [TestCase(0.5, "Imperial", 0.19 )]
     [TestCase(1, "Imperial", 0.39)]
     [TestCase(1000, "Imperial",393.70)]
-    public void Convert_GivenMetric_ReturnsImperial(double numberInCm, string convertToInches, decimal expectedInInches)
+    public void ConvertCM_ToImperial_ReturnsInches(double numberInCm, string convertToInches, decimal expectedInInches)
     {
         // Arrange
         var length = new Length(numberInCm, convertToInches);
@@ -26,10 +26,10 @@ public class LengthTests
     [TestCase(3.94, "Metric", 10)]
     [TestCase(0.39, "Metric" , 0.99)]
     [TestCase(1000, "Metric", 2540)]
-    public void ConvertLength_GivenImperial_ReturnsMetric(double numberInInches, string convertToCM, decimal expectedInCm)
+    public void ConvertInches_ToMetric_ReturnsCM(double numberInInches, string convertToMetric, decimal expectedInCm)
     {
         // Arrange
-        var length = new Length(numberInInches, convertToCM);
+        var length = new Length(numberInInches, convertToMetric);
         
         // Act
         var result = length.Convert();
